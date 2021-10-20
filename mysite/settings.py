e73238bdd9f4c496d986bbda36a401e19d831986
xuanhuaz <kaowofeixiang@gmail.com>
2021-10-20 02:14:32 -0400

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,5 +123,4 @@ STATIC_URL = "/static/"
 
 # Activate Django-Heroku.
 django_heroku.settings(locals(), test_runner=False)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
