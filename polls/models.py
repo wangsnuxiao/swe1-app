711ca8445288dfb8a8cfd9b08b08c50c9b815815
xuanhuaz <kaowofeixiang@gmail.com>
2021-10-20 01:08:56 -0400

    black reformatted

--- polls/models.py
+++ polls/models.py
@@ -7,19 +7,17 @@ from django.db import models
 from django.utils import timezone
 
 
-
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.question_text
 
     @admin.display(
         boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
+        ordering="pub_date",
+        description="Published recently?",
     )
     def was_published_recently(self):
         now = timezone.now()
